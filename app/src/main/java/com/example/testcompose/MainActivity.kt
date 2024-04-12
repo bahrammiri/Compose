194ec0d6b38527d0c4f1c911d5257232651ad5a9
Bahram Miri <bahram@MacBook-Pro.local>
2024-04-12 19:17:29 +0330

    Initial

--- app/src/main/java/com/example/testcompose/MainActivity.kt
+++ app/src/main/java/com/example/testcompose/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.testcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -13,31 +14,18 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.testcompose.ui.theme.TestComposeTheme
 
 class MainActivity : ComponentActivity() {
+
+    val numberViewModel by viewModels<NumberViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             TestComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    NumberScreen(numberViewModel)
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TestComposeTheme {
-        Greeting("Android")
-    }
-}
---
+++ app/src/main/java/com/example/testcompose/NumberScreen.kt
@@ -0,0 +1,30 @@
+package com.example.testcompose
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun NumberScreen(numberViewModel: NumberViewModel) {
+
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = {
+
+            numberViewModel.generateNumber()
+        }) {
+            Text(numberViewModel.number.value.toString())
+        }
+
+    }
+
+
+}
