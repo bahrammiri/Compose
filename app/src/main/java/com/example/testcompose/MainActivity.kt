5e768b745e8d84cd822a95adbba9d01c91a89780
Bahram Miri <bahram@MacBook-Pro.local>
2024-04-12 19:17:29 +0330

    Initial

--- app/src/main/java/com/example/testcompose/MainActivity.kt
+++ app/src/main/java/com/example/testcompose/MainActivity.kt
@@ -14,7 +14,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.testcompose.ui.theme.TestComposeTheme
 
 class MainActivity : ComponentActivity() {
-
+//master
     val numberViewModel by viewModels<NumberViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
